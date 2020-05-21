31e207a9b6132976f530f97f25dc599593f01186
Silvio <silviolima@gmail.com>
2020-05-21 11:54:04 -0300

    Update

--- app.py
+++ app.py
@@ -21,14 +21,14 @@ def read_digits():
     return load_digits()
 
 def plot_digits(digits):
-    fig, axes = plt.subplots(4,10, figsize=(10, 4), subplot_kw= { 'xticks':[], 'yticks':[]}, gridspec_kw=dict(hspace = 0.95, wspace=0.05))
+    fig, axes = plt.subplots(4,10, figsize=(10, 6), subplot_kw= { 'xticks':[], 'yticks':[]}, gridspec_kw=dict(hspace = 0.95, wspace=0.05))
     for i, ax in enumerate(axes.flat):
         ax.imshow(digits.images[i].reshape(8,8),cmap='binary', interpolation='nearest', clim=(0,16))
         ax.text(0,9, str(digits.target[i]))
     st.pyplot()
 
 def plot_noisy(noisy,digits):
-    fig, axes = plt.subplots(4,10, figsize=(10, 4), subplot_kw= { 'xticks':[], 'yticks':[]}, gridspec_kw=dict(hspace = 0.95, wspace=0.05))
+    fig, axes = plt.subplots(4,10, figsize=(10, 6), subplot_kw= { 'xticks':[], 'yticks':[]}, gridspec_kw=dict(hspace = 0.95, wspace=0.05))
     for i, ax in enumerate(axes.flat):
         ax.imshow(noisy[i].reshape(8,8),cmap='binary', interpolation='nearest', clim=(0,16))
         ax.text(0,9, str(digits.target[i]))
@@ -45,6 +45,7 @@ def plot_pca(project,digits):
     fig = plt.scatter(x = x, y = y, c=digits.target, cmap="Paired")
     plt.colorbar()
     st.pyplot()
+
 
 def main():
     """Principal Components Analysis  App """
@@ -67,26 +68,25 @@ def main():
               """
     st.markdown(html2_page, unsafe_allow_html=True)
     
-    
      
     image = Image.open("digits.png")
     st.sidebar.image(image,caption="", use_column_width=True)
 
-    activities = ["Home","Digits","PCA","Number of Components","Noisy","PCA as Noise Filtering","About"]
-    choice = st.sidebar.selectbox("Home",activities)
+    activities = ["Home","Digits","Number of Components","PCA to Reduce The Number of Dimensions","PCA as Noisy Filtering","About"]
+    choice = st.sidebar.selectbox("Menu",activities)
     # Using digits read from datasets
     digits = read_digits()
      
     if choice == 'Home':
         st.subheader("Principal Component Analysis")
-        st.write("Definitions")
+        st.markdown("**Definitions**")
         st.write("PCA is not a statistical method to infer parameters or test hypotheses. Instead, it provides a method to reduce a complex dataset to lower dimension to reveal sometimes hidden, simplified structure that often underlie it.")
         st.write("")
         st.write("PCA is a statistical method routinely used to analyze interrelationships among large numbers of objects.")
         st.write("")
         st.write("Principal component analysis (PCA) is a mathematical algorithm that reduces the dimensionality of the data while retaining most of the variation in the data set.")
         
-        if st.button("visit page"):
+        if st.button("Learn more about it"):
             js = "window.open('https://jakevdp.github.io/PythonDataScienceHandbook/05.09-principal-component-analysis.html')"
             html = '<img src onerror="{}">'.format(js)
             div = Div(text=html)
@@ -97,22 +97,29 @@ def main():
 
     elif choice == "Digits":
         st.subheader("Demonstration")
-        st.write("Shape:",digits.data.shape)
+        st.markdown("Applying **PCA**")
+        st.markdown("- **to reduce number of dimension**")
+        st.markdown("- **to filter noisy**")
+        st.write("Original Shape:",digits.data.shape)
         st.write("Dimensions:",digits.data.shape[1])
         plot_digits(digits)
         
-    elif choice == "PCA":
+    elif choice == "PCA to Reduce The Number of Dimensions":
         st.subheader("Applying PCA")
         # Appplying pca to transform in 2 dimensions
         pca = PCA(2)
         project = pca.fit_transform(digits.data)
-        st.write("From 64 to 2 dimensions using PCA...")
+        st.write("")
+        st.markdown("From **64** to **2** dimensions using PCA...")
         st.write("Shape:",project.shape)
         plot_pca(project,digits)
 
     elif choice == "Number of Components":
         st.subheader("Number of components")
-        pca = PCA().fit(digits.data)
+        st.markdown("Dataset Digits has **64** dimensions to represent the number from 0 to 9")
+        st.markdown("**PCA** can help to reduce it. You can inform the **number of components** or the **%rate of variance** to be preserved in new components")
+        passagem = st.sidebar.slider("Choose Number of components",min_value=0, max_value=64, value=10, step=5)
+        pca = PCA(passagem).fit(digits.data)
         plt.plot(np.cumsum(pca.explained_variance_ratio_))
         plt.xlabel("Number of components")
         plt.ylabel("Cumulative explained variance")
@@ -120,33 +127,29 @@ def main():
         st.write("This curve quantifies how much of the total, 64-dimensional variance is contained within the first N components. For example, we see that with the digits the first 10 components contain approximately 75% of the variance, while you need around 50 components to describe close to 100% of the variance.")
         st.write("Using only two-dimensional projection it loses a lot of information (as measured by the explained variance) and that we'd need about 20 components to retain 90% of the variance.")
 
-    elif choice == "Noisy":
-        st.subheader("Noisy")
-        st.subheader("Original dataset digits")
+    elif choice == "PCA as Noisy Filtering":
+        st.subheader("PCA as Noisy Filtering")
+        st.markdown("**Original dataset digits**")
         #st.write("Shape:",digits.data.shape)
         #st.write("Dimensions:",digits.data.shape[1])
         plot_digits(digits)
-
-        noisy = np.random.normal(digits.data,4)
-        st.subheader("Dataset digits with noisy")
+        
         #st.write("Shape:",noisy.shape)
         #st.write("Dimensions:",noisy.shape[1])
+        noisy_factor = st.sidebar.slider('Noisy Factor ',min_value=0, max_value=10, value=4, step=1)
+        noisy = np.random.normal(digits.data,noisy_factor)
+        st.markdown("**Dataset digits with noisy**")
+        st.write("Noisy:",noisy_factor)
         plot_noisy(noisy,digits)
-   
-        pca = PCA(0.5).fit(noisy)
         
-     
-    elif choice == "PCA as Noise Filtering":
-        st.subheader("PCA as Noise Filtering")
-        st.write("Dataset original")
-        plot_digits(digits)
-        st.write("Dataset with noisy")
-        noisy = np.random.normal(digits.data,5)
-        plot_noisy(noisy,digits)
-        pca = PCA(0.5).fit(noisy)
+        variance = st.sidebar.slider('% Rate of variance to retain',min_value=0.1, max_value=0.99, value=0.5, step=0.1)
+        pca = PCA(variance).fit(noisy)
         components = pca.transform(noisy)
         filtered = pca.inverse_transform(components)
-        st.write("Dataset after PCA applyed as noise filtering")
+        st.markdown("**Dataset after PCA applyed as noise filtering**")
+        rate = str(100* variance)+"%"
+        st.write("Rate of variance:",rate)
+        st.write("Number of components used: "+ str(pca.n_components_))
         plot_noisy(filtered,digits)
 
     elif choice == 'About':
